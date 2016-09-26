6d7185f877b5ee986064c777f645bfa68fd01479
Trey Evans <lewis.r.evans@gmail.com>
2016-09-26 10:17:38 -0400

    Added capistrano.

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Unicorn as the app server
 
 # Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -41,6 +40,8 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'capistrano-rails', '1.1.7'
+  gem 'capistrano', '3.5.0'
 end
 
 group :production do
---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, "openhbx_enterprise_extractor"
+set :repo_url, "https://github.com/dchbx/openhbx_enteprise_extractor.git"
+set :user, "nginx"
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/deployments/openhbx_enterprise_extractor'
+
+set :bundle_binstubs, false
+set :bundle_flags, "--quiet"
+set :bundle_path, nil
+
+# Default value for :pty is false
+set :pty, true
+
+# set :rails_env, "production"
+# Default value for :scm is :git
+# set :scm, :git
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/sneakers.conf.rb', 'config/settings.yml', 'eyes/openhbx_enterprise_extractor.eye.rb')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'pids', 'eye')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 20 do
+      sudo "service eye_openhbx_enterprise_extractor reload"
+    end
+  end
+end
+
+after "deploy:publishing", "deploy:restart"
