cbf0176f3e0ac8e729fdadb92ffd5cfbc7dabff6
Matt Williams <matt.williams2@dc.gov>
2016-10-06 14:12:00 -0400

    add mongoid.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :pty, true
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/sneakers.conf.rb', 'config/settings.yml', 'eyes/openhbx_enterprise_extractor.eye.rb')
+set :linked_files, fetch(:linked_files, []).push('config/sneakers.conf.rb', 'config/settings.yml', 'eyes/openhbx_enterprise_extractor.eye.rb', 'config/mongoid.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'pids', 'eye')
